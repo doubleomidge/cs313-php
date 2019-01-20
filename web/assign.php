46631ce25aebbfbbfb9b0893dca7f1b90f70ee62
doubleomidge <doubleomidge@gmail.com>
2019-01-19 23:46:03 -0700

    web 2 homepage

--- web/assign.php
+++ web/assign.php
@@ -6,11 +6,12 @@
     <title>Abby's Projects</title>
 
     <!-- bootstrap -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
         crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
         crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="main.css">
@@ -36,7 +37,8 @@
         <div class="col project recent1">
             <div class="pro-content">
                 <h2>A homepage interface</h2>
-                <p>The homepage and setup for this site was the first step to diving deeper into web development.</p>
+                <p>The homepage and setup for
+                    <a href="https://vast-reaches-29790.herokuapp.com/hello.php">this site</a> was the first step to diving deeper into web development.</p>
                 <p>The page was built using Bootstrap 4 and SASS.</p>
                 <p>All changes and updates to this page were pushed to GitHub through the terminal.</p>
             </div>
--- web/nav.php
+++ web/nav.php
@@ -1,22 +1,20 @@
 <nav class="navbar navbar-default navbar-expand-lg">
-		<a class="navbar-brand" href="#">Vast Reaches</a>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText"
-		 aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse" id="navbarText">
-			<ul class="navbar-nav mr-auto">
-				<li class="nav-item active">
-					<a class="nav-link" href="hello.php">Home
-						<span class="sr-only">(current)</span>
-					</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="hello.php#about">About</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="assign.php">Projects</a>
-				</li>
-			</ul>
-		</div>
-	</nav>
+    <a class="navbar-brand" href="#">Vast Reaches</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
+        aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="hello.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="hello.php#about">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="assign.php">Projects</a>
+            </li>
+        </ul>
+    </div>
+</nav>
