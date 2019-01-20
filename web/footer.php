0f485603f116e4a37eb1310461f7e32b136cdd3e
doubleomidge <doubleomidge@gmail.com>
2019-01-19 21:52:43 -0700

    s

--- web/footer.php
+++ web/footer.php
@@ -1,6 +1,6 @@
 <footer class="foot">
 		<div class="footer-copyright text-center">
 			<span>© 2019 Abby Loosle </span>
-			<p>Thanks to Bootstrap and FontAwesome for the layout and icons.</p>
+			<p>Thanks to <a href="https://getbootstrap.com/" class="subtle-link">Bootstrap</a> and <a href="https://fontawesome.com/" class="subtle-link">FontAwesome</a> for the layout and icons.</p>
 		</div>
 	</footer>
