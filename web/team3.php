3919f46689b070afabd95ca008e9da34e9cd3145
doubleomidge <doubleomidge@gmail.com>
2019-01-22 11:06:08 -0700

    countries dictionary

--- web/team3.php
+++ web/team3.php
@@ -52,7 +52,7 @@ $countries = array(
 		foreach($countries as $key=>$country) {
 			echo '<div class="form-check form-check-inline">
   			<input class="form-check-input" type="checkbox" name="travel[]" id=" ' . $country . ' " value=" ' . $country . ' ">
-  			<label class="form-check-label" for="inlineCheckbox1"> ' . $country . ' a</label>
+  			<label class="form-check-label" for="inlineCheckbox1"> ' . $country . ' </label>
 			</div>';
 		}
 
