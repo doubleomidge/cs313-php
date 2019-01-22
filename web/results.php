0e7dc26ba8ff5274d3e632f62401389f548e2a4b
doubleomidge <doubleomidge@gmail.com>
2019-01-22 10:37:51 -0700

    places

--- web/results.php
+++ web/results.php
@@ -11,15 +11,23 @@ $userTravel = filter_input(INPUT_POST, 'travel');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- bootstrap -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+	 crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+	 crossorigin="anonymous"></script>
     <title>User Results</title>
 </head>
+
 <body>
     <div class="container">
         <h1>Hi <?php echo $userName ?></h1>
         <p>This is your email link: <a href="mailto:<?php echo $userEmail ?>"><?php echo $userEmail ?></a></p>
         <p>Your major is: <?php echo $userMajor ?></p>
         <p>You said: <?php echo $userComments ?></p>
-        <p>You've been to: <?php echo $userTravel ?></p>
+        <p>You've been to: <?php foreach($userTravel as $places){echo $places;} ?></p>
     </div>
 </body>
 </html>
