c6df4f95d54f06c8313bc378caf824944d1979c9
doubleomidge <doubleomidge@gmail.com>
2019-01-24 11:14:05 -0700

    shopping

--- web/shopping/controller.php
+++ web/shopping/controller.php
@@ -5,12 +5,12 @@ session_start();
 
 // make array of items
 $items = array(
-    array('id'=>1, 'itemName'=>'Hula Duck', 'image'=>'img/hula-duck.jpeg', 'price'=>4.25, 'cart'=>False),
-    array('id'=>2, 'itemName'=>'Glow Duck', 'image'=>'img/glow-duck.jpeg', 'price'=>4.50, 'cart'=>False),
-    array('id'=>3, 'itemName'=>'St. Patty Duck', 'image'=>'img/patty-duck.jpeg', 'price'=>4.25, 'cart'=>False),
-    array('id'=>4, 'itemName'=>'Pirate Duck', 'image'=>'img/pirate-duck.jpeg', 'price'=>4.25, 'cart'=>False),
-    array('id'=>5, 'itemName'=>'Sailor Duck', 'image'=>'img/sail-duck.jpeg', 'price'=>4.25, 'cart'=>False),
-    array('id'=>6, 'itemName'=>'Spa Duck', 'image'=>'img/spa-duck.jpeg', 'price'=>4.25, 'cart'=>False),
+    array('itemId'=>1, 'itemName'=>'Hula Duck', 'image'=>'img/hula-duck.jpeg', 'price'=>4.25, 'cart'=>False),
+    array('itemId'=>2, 'itemName'=>'Glow Duck', 'image'=>'img/glow-duck.jpeg', 'price'=>4.50, 'cart'=>False),
+    array('itemId'=>3, 'itemName'=>'St. Patty Duck', 'image'=>'img/patty-duck.jpeg', 'price'=>4.25, 'cart'=>False),
+    array('itemId'=>4, 'itemName'=>'Pirate Duck', 'image'=>'img/pirate-duck.jpeg', 'price'=>4.25, 'cart'=>False),
+    array('itemId'=>5, 'itemName'=>'Sailor Duck', 'image'=>'img/sail-duck.jpeg', 'price'=>4.25, 'cart'=>False),
+    array('itemId'=>6, 'itemName'=>'Spa Duck', 'image'=>'img/spa-duck.jpeg', 'price'=>4.25, 'cart'=>False),
 );
 
 $action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);
@@ -20,7 +20,15 @@ if ($action == NULL) {
 
 switch ($action) {
     case 'addToCart':
-        $itemName = filter_input(INPUT_GET, 'item', FILTER_SANITIZE_STRING);
+        $itemId = filter_input(INPUT_GET, 'item', FILTER_SANITIZE_NUMBER_INT);
+
+        if($itemName != "") {
+            if($_SESSION['cart'] == "") {
+                $_SESSION['cart'] = array($items[$itemId]);
+            } else {
+                array_push($_SESSION['cart'], $items[$itemId]);
+            }
+        }
 
 
         // post message to page saying item has been added
