6a281541663108c4fc99a9e7888240ec1a2d3c4b
doubleomidge <doubleomidge@gmail.com>
2019-01-31 11:06:41 -0700

    add project

--- web/footer.php
+++ web/footer.php
@@ -2,6 +2,6 @@
 		<div class="footer-copyright text-center">
 			<span>© 2019 Abby Loosle </span>
 			<p>Thanks to <a href="https://getbootstrap.com/" class="subtle-link">Bootstrap</a> and <a href="https://fontawesome.com/" class="subtle-link">FontAwesome</a> for the layout and icons.</p>
-			<p>Oh and thanks to <a href="https://unsplash.com/photos/sVhRMCyo1_Y">Artem Bali</a> for supplying the photo on Unsplash.com .</p>
+			<p>Oh and thanks to <a href="https://unsplash.com/photos/sVhRMCyo1_Y">Artem Bali</a> for supplying the photo on Unsplash.</p>
 		</div>
 	</footer>
