20b087c5b76b81915eaae6444b6003b5d8f49ba6
doubleomidge <doubleomidge@gmail.com>
2019-02-02 22:58:31 -0700

    add SQL file

--- web/movie.sql
+++ web/movie.sql
@@ -1,24 +1,24 @@
-CREATE TABLE User {
-    user_id          STATIC          NOT NULL,
+CREATE TABLE Users (
+    user_id         SERIAL          NOT NULL,
     username        varchar(15)     NOT NULL,
     user_firstname  varchar(20)     NOT NULL,
     user_lastname   varchar(20)     NOT NULL,
     user_password   varchar(15)     NOT NULL,
     family_id       int             NOT NULL,
-    PRIMARY KEY (userId)
-};
+    PRIMARY KEY (user_id)
+);
 
-CREATE TABLE Family {
-    family_id       STATIC          NOT NULL,
+CREATE TABLE Family (
+    family_id       SERIAL          NOT NULL,
     family_name     varchar(30)     NOT NULL,
     PRIMARY KEY (family_id)
-};
+);
 
-ALTER TABLE User
+ALTER TABLE Users
 ADD FOREIGN KEY (family_id) REFERENCES Family(family_id);
 
-CREATE TABLE Movies {
-    movie_id        STATIC          NOT NULL,
+CREATE TABLE Movies (
+    movie_id        SERIAL          NOT NULL,
     movie_title     varchar(30)     NOT NULL,
     movie_year      varchar(20)     NOT NULL,
     movie_desc      varchar(500)    NOT NULL,
@@ -30,28 +30,28 @@ CREATE TABLE Movies {
     user_id         int             NOT NULL,
     location_id     int             NOT NULL,
     PRIMARY KEY (movie_id)
-};
+);
 
-CREATE TABLE Genre {
-    genre_id        STATIC          NOT NULL,
+CREATE TABLE Genre (
+    genre_id        SERIAL          NOT NULL,
     genre_name      varchar(100)    NOT NULL,
     PRIMARY KEY (genre_id)
-};
+);
 
-CREATE TABLE Location {
-    location_id     STATIC          NOT NULL,
+CREATE TABLE Location (
+    location_id     SERIAL          NOT NULL,
     location_name   varchar(100)    NOT NULL,
     PRIMARY KEY (location_id)
-};
+);
 
-ALTER TABLE User
+ALTER TABLE Movies
 ADD FOREIGN KEY (genre_id) REFERENCES Genre(genre_id);
 
-ALTER TABLE Movie
+ALTER TABLE Movies
 ADD FOREIGN KEY (family_id) REFERENCES Family(family_id);
 
-ALTER TABLE Movie
-ADD FOREIGN KEY (user_id) REFERENCES User(user_id);
+ALTER TABLE Movies
+ADD FOREIGN KEY (user_id) REFERENCES Users(user_id);
 
-ALTER TABLE Movie
+ALTER TABLE Movies
 ADD FOREIGN KEY (location_id) REFERENCES Location(location_id);
