9b7c0e8b44169e5e56cb5fcc5ba135aef44c90fc
doubleomidge <doubleomidge@gmail.com>
2019-02-14 10:51:06 -0700

    add movie

--- web/movie/functions.php
+++ web/movie/functions.php
@@ -52,7 +52,7 @@ function ratingList($ratings){
 
 function addMovie($title, $desc, $year, $movieb, $digitalb, $run, $rate, $gen, $type) {
     $db = dbConnect();
-    echo "$title, $desc, $year, $movieb, $digitalb, $run, $rate, $gen, $type";
+    // echo "$title, $desc, $year, $movieb, $digitalb, $run, $rate, $gen, $type";
     $sql = 'INSERT INTO Movies VALUES(DEFAULT, :title, :year, :desc, :digitalb, :movieb, :run, :rate, :gen, 1, 1, :type)';
     $stmt = $db->prepare($sql);
     $stmt->bindValue(':title', $title, PDO::PARAM_STRING);
@@ -66,6 +66,6 @@ function addMovie($title, $desc, $year, $movieb, $digitalb, $run, $rate, $gen, $
     $stmt->bindValue(':type', $type, PDO::PARAM_STRING);
     $stmt->execute();
     $movieInfo = $stmt->rowCount();
-    echo $movieInfo;
+    // echo $movieInfo;
     return $movieInfo;
 };
