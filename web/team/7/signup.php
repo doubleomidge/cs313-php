85ad6c238dc93188e97ea3f013bca2ac5282b3a8
doubleomidge <doubleomidge@gmail.com>
2019-02-19 10:26:18 -0700

    team 7

--- web/team/7/signup.php
+++ web/team/7/signup.php
@@ -28,16 +28,6 @@
     <div class="container register card">
         <form method="post" action="index.php?action=register">
             <h1 class="bump-center">Register</h1>
-            <div class="row">
-                <div class="col-md-6 mb-3">
-                    <label for="firstName">First Name</label>
-                    <input type="text" class="form-control" id="firstName" name="firstname" required>
-                </div>
-                <div class="col-md-6 mb-3">
-                    <label for="lastName">Last Name</label>
-                    <input type="text" class="form-control" id="lastName" name="lastname" required>
-                </div>
-            </div>
 
             <div class="mb-3">
                 <label for="username">Username</label>
@@ -45,18 +35,8 @@
             </div>
 
             <div class="mb-3">
-                <label for="email">Email</label>
-                <input type="text" class="form-control" id="email" name="email" placeholder="you@example.com" required>
-            </div>
-
-            <div class="mb-3">
                 <label for="password">Password</label>
                 <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
-            </div>
-
-            <div class="mb-3">
-                <label for="password-confirm">Confirm Password</label>
-                <input type="password" class="form-control" id="password-confirm" name="password_confirm" placeholder="Confirm Password" required>
             </div>
 
             <button class="btn btn-primary btn-lg btn-block register-button" type="submit">Register Now</button>
