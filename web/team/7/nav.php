96215941254fe921ebf6d862eebed533b4fa5e99
doubleomidge <doubleomidge@gmail.com>
2019-02-19 10:20:04 -0700

    team 7

--- web/team/7/nav.php
+++ web/team/7/nav.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
-    <a class="navbar-brand" href="#" class="headerfont">Team 7</a>
+    <a class="navbar-brand" href="welcome.php" class="headerfont">Team 7</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
         aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
