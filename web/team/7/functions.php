632bcd42570444452efc33b244030020697e16e0
doubleomidge <doubleomidge@gmail.com>
2019-02-19 10:52:40 -0700

    team 7

--- web/team/7/functions.php
+++ web/team/7/functions.php
@@ -5,7 +5,7 @@ function addUser ($username, $safepassword) {
     $sql = 'INSERT INTO User7 VALUES(DEFAULT, :username, :password)';
     $stmt = $db->prepare($sql);
     $stmt->bindValue(':username', $username, PDO::PARAM_STR);
-    $stmt->bindValue(':password', $password, PDO::PARAM_STR);
+    $stmt->bindValue(':password', $safepassword, PDO::PARAM_STR);
     $stmt->execute();
     $add = $stmt->rowCount();
     return $add;
