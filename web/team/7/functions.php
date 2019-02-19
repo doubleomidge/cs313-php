2c483e6274e8a00c5f003abfe7752ce925d6410f
doubleomidge <doubleomidge@gmail.com>
2019-02-19 10:53:45 -0700

    team 7

--- web/team/7/functions.php
+++ web/team/7/functions.php
@@ -1,5 +1,30 @@
 <?php
 
+function dbConnect() {
+    try
+  {
+    $dbUrl = getenv('DATABASE_URL');
+
+    $dbOpts = parse_url($dbUrl);
+
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  return $db;
+}
+  catch (PDOException $ex)
+  {
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+  }
+};
+
 function addUser ($username, $safepassword) {
     $db = dbConnect();
     $sql = 'INSERT INTO User7 VALUES(DEFAULT, :username, :password)';
