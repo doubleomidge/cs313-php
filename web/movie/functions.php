f7a3fa858a1c5b5081695f0560619c70f4242f37
doubleomidge <doubleomidge@gmail.com>
2019-02-20 14:50:28 -0700

    work on signup/login process

--- web/movie/functions.php
+++ web/movie/functions.php
@@ -154,7 +154,7 @@ function addUser($username, $firstname, $lastname, $email, $safepass) {
 
 function getPassword ($username) {
     $db = dbConnect();
-    $sql = 'SELECT * FROM User WHERE username = :username';
+    $sql = 'SELECT * FROM Users WHERE username = :username';
     $stmt = $db->prepare($sql);
     $stmt->bindValue(':username', $username, PDO::PARAM_STR);
     $stmt->execute();
