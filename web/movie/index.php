45753c1ded1a93623f7a1270b3020766d1637480
doubleomidge <doubleomidge@gmail.com>
2019-02-20 10:33:36 -0700

    rearrange into folders

--- web/movie/index.php
+++ web/movie/index.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once ('/model/dbconnect.php');
-require_once ('/model/functions.php');
+require_once ('../model/dbconnect.php');
+require_once ('../model/functions.php');
 
 
 $action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);
