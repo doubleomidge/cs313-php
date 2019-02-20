1b19f0327483976d7f4dfcb36929592dd2999aa0
doubleomidge <doubleomidge@gmail.com>
2019-02-20 13:10:31 -0700

    work on signup/login process

--- web/movie/index.php
+++ web/movie/index.php
@@ -239,7 +239,7 @@ switch ($action) {
         if ($passcomp != 0) {
             $passMessage = "<p style='color: red; text-align: center;'> Sorry, there was an error logging you in.</p>";
             $star = "<span style='color: red;'>*</span>";
-            include '../view/signup.php';
+            include '/view/signup.php';
         } else {
             // $verify = checkPassword($password);
             
@@ -251,7 +251,7 @@ switch ($action) {
 
             $safepass = password_hash($password, PASSWORD_DEFAULT);
             $added = addUser($username, $firstname, $lastname, $email, $safepass);
-            include '../view/login.php';
+            include '/view/login.php';
         }
         
         
@@ -268,7 +268,7 @@ switch ($action) {
         break;
 
     default: 
-    include '../view/home.php';
+    include '/view/home.php';
 
 }
 
