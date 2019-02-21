fa7e8e3156ff26d2302ac561a6ea5b27f065034e
doubleomidge <doubleomidge@gmail.com>
2019-02-21 14:06:17 -0700

    restrict view of non login

--- web/movie/functions.php
+++ web/movie/functions.php
@@ -172,11 +172,11 @@ function checkPassword($clientPassword) {
 
 function getAllMovieDetails($movieId){
     $db = dbConnect();
-    $sql = 'SELECT * FROM Movies m WHERE movie_id = :movie_id
+    $sql = 'SELECT * FROM Movies m
                 JOIN Genre g ON m.genre_id = g.genre_id
                 JOIN Rating r ON m.movie_rating_id = r.movie_rating_id
-                JOIN Format f ON m.format_id = f.format_id
-            WHERE movie_id = :movie_id';
+                JOIN `Format` f ON m.format_id = f.format_id
+            WHERE m.movie_id = :movie_id';
     $stmt = $db->prepare($sql);
     $stmt->bindValue(':movie_id', $movieId, PDO::PARAM_INT);
     $stmt->execute();
