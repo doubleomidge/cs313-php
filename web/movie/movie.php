5d4321699ff81e3797a57b7a62f09eeb3982d7d2
doubleomidge <doubleomidge@gmail.com>
2019-02-22 11:59:22 -0700

    genre list

--- web/movie/movie.php
+++ web/movie/movie.php
@@ -5,16 +5,16 @@ require 'dbconnect.php';
 if(!isset($_GET['action'])) {
     $moviejoin = 'SELECT * FROM Movies m
                 JOIN Rating r ON m.movie_rating_id = r.rating_id
-                JOIN Genre g ON m.genre_id = g.genre_id
                 JOIN Genre_movie gm ON m.movie_id = gm.movie_id
+                JOIN Genre g ON gm.genre_id = g.genre_id
                 JOIN Format f on m.format_id = f.format_id';
 } else {
     $column = $_GET['action'];
 
     $moviejoin = "SELECT * FROM Movies m
                 JOIN Rating r ON m.movie_rating_id = r.rating_id
-                JOIN Genre g ON m.genre_id = g.genre_id
                 JOIN Genre_movie gm ON m.movie_id = gm.movie_id
+                JOIN Genre g ON gm.genre_id = g.genre_id
                 JOIN Format f on m.format_id = f.format_id
                 ORDER BY $column ASC";
     // echo $moviejoin;
